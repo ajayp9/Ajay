6a527ba13a694ad55dc63a8f865afb36b97804c5
Ajay Pal <124481153+ajayp9@users.noreply.github.com>
2025-07-17 02:16:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,3 +36,16 @@ function toggleTheme(){
     localStorage.theme = 'light';
   }
 }
+
+
+document.addEventListener("mousemove", function(e) {
+  const trail = document.createElement("div");
+  trail.className = "cursor-trail";
+  trail.style.left = e.pageX + "px";
+  trail.style.top = e.pageY + "px";
+  document.body.appendChild(trail);
+
+  setTimeout(() => {
+    trail.remove();
+  }, 800); // should match animation duration
+});
